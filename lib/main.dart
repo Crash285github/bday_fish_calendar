78375d4c2dbd31aeae1e83fd30349e8886b6a5eb
Crash285 <theoriginalcrash285@gmail.com>
2026-09-16 20:07:19 +0200

    analysis

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import "package:bday_fish_calendar/theme.dart";
+import "package:flutter/material.dart";
 
 void main() {
   runApp(const MainApp());
@@ -8,13 +9,8 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    theme: appTheme,
+    home: const Scaffold(body: Center(child: Text("Happy Birthday!"))),
+  );
 }
---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import "package:flutter/material.dart";
+
+final ThemeData appTheme = ThemeData.from(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color(0xffaf01fa),
+    brightness: Brightness.dark,
+  ),
+);
